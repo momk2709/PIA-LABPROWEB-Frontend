61ccac258ac62f3878119285caf0335ce5553d5c
Karen_Mora <momk2709@gmail.com>
2023-05-27 03:02:50 -0600

    Masomenos aun

--- src/utils/inSession.js
+++ src/utils/inSession.js
@@ -6,16 +6,20 @@ const informeTab = document.getElementById("informes");
 const menu = document.getElementById("menu");
 
 window.addEventListener("DOMContentLoaded", async () => {
-  const session = await getSession();
+  try {
+    const session = await getSession();
 
-  if (!session) {
-    registerButton.classList.remove("d-none");
-    loginButton.classList.remove("d-none");
-    informeTab.classList.remove("d-none");
-    menu.classList.add("d-none");
-  } else {
-    registerButton.classList.add("d-none");
-    loginButton.classList.add("d-none");
-    informeTab.classList.add("d-none");
+    if (!session) {
+      registerButton.classList.remove("d-none");
+      loginButton.classList.remove("d-none");
+      informeTab.classList.remove("d-none");
+      menu.classList.add("d-none");
+    } else {
+      registerButton.classList.add("d-none");
+      loginButton.classList.add("d-none");
+      informeTab.classList.add("d-none");
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
