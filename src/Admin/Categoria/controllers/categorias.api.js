616aa89e9bc3f492d120d0441b60084653dddf1c
Karen_Mora <momk2709@gmail.com>
2023-05-30 11:03:34 -0600

    admin categorias

--- src/Admin/Categoria/controllers/categorias.api.js
+++ src/Admin/Categoria/controllers/categorias.api.js
@@ -5,33 +5,29 @@ import { getApiError } from "../../../utils/getApiError";
 // Esto es como lo que hacen en postman para probar las rutas
 // Solo que aqui se hace con codigo
 export async function getAllCategorias() {
-    try {
-        const response = await cursosApi.get("/admin/categories");
+  try {
+    const response = await cursosApi.get("/admin/categories");
 
-        return response.data.data;
-    } catch (error) {
-        throw getApiError(error);
-    }
+    return response.data.data;
+  } catch (error) {
+    throw getApiError(error);
+  }
 }
 
 // ESTE ES EL ENDPOINT PARA CREAR UNA CATEGORIA, como pueden ver le pasamos los datos que necesitamos
 // los datos los pueden encontrar en el POSTMAN
-export async function createCategories({
-    nombre,
-    descripcion,
-    imagenUrl,
-}) {
-    try {
-        const response = await cursosApi.post("/admin/categories", {
-            nombre,
-            descripcion,
-            imagenUrl,
-        });
+export async function createCategories({ nombre, descripcion, imagenUrl }) {
+  try {
+    const response = await cursosApi.post("/admin/categories", {
+      nombre,
+      descripcion,
+      imagenUrl,
+    });
 
-        return response.data.data;
-    } catch (error) {
-        throw getApiError(error);
-    }
+    return response.data.data;
+  } catch (error) {
+    throw getApiError(error);
+  }
 }
 
 // ESTE ES EL ENDPOINT PARA ACTUALIZAR UNA CATEGORIA, como pueden ver le pasamos los datos que necesitamos
@@ -41,32 +37,40 @@ export async function createCategories({
 
 // En este caso no se actualiza el id, pero si se actualiza el nombre, descripcion, precio, fecha_inicio y fecha_fin
 // ACTUALIZAR
-export async function updateCategories({
-    id,
-    nombre,
-    descripcion,
-    imagenUrl,
-}) {
-    try {
-        const response = await cursosApi.put(`/admin/categories/${id}`, {
-            nombre,
-            descripcion,
-            imagenUrl,
-        });
+export async function updateCategories({ id, nombre, descripcion, imagenUrl }) {
+  try {
+    const response = await cursosApi.put(`/admin/categories/${id}`, {
+      nombre,
+      descripcion,
+      imagenUrl,
+    });
 
-        return response.data.data;
-    } catch (error) {
-        throw getApiError(error);
-    }
+    return response.data.data;
+  } catch (error) {
+    throw getApiError(error);
+  }
 }
 
 // ELIMINAR
 export async function deleteCategories(id) {
-    try {
-        const response = await cursosApi.delete(`/admin/categories/${id}`);
+  try {
+    const response = await cursosApi.delete(`/admin/categories/${id}`);
 
-        return response.data.data;
-    } catch (error) {
-        throw getApiError(error);
-    }
+    return response.data.data;
+  } catch (error) {
+    throw getApiError(error);
+  }
+}
+
+export async function asignarCategoria({ categoria_id, curso_id }) {
+  try {
+    const response = await cursosApi.post(`/admin/asignacion/curso-category`, {
+      categoria_id,
+      curso_id,
+    });
+
+    return response.data.data;
+  } catch (error) {
+    throw getApiError(error);
+  }
 }
--- src/Admin/Categoria/controllers/categorias.events.js
+++ src/Admin/Categoria/controllers/categorias.events.js
@@ -3,15 +3,19 @@
 // Estas son las funciones que se comunican con el backend
 // Las definimos en otro archivo para que este archivo no se haga tan largo
 import {
-    getAllCategorias,
-    createCategories,
-    updateCategories,
-    deleteCategories,
+  getAllCategorias,
+  createCategories,
+  updateCategories,
+  deleteCategories,
+  asignarCategoria,
 } from "./categorias.api";
 
 // Necesitamos los modales, RECUERDEN SUS IDS
 const editarCategoriaModal = document.getElementById("editar-categoria-modal");
 const crearCategoriaModal = document.getElementById("modal-crear");
+const asginarCategoriaModal = document.getElementById(
+  "asignar-categoria-modal"
+);
 
 //Estas son librerias que nos facilitan el hacer las tablas y alertas
 
@@ -21,144 +25,161 @@ import format from "date-fns/format";
 
 // Este es el evento que se ejecuta cuando se carga la pagina, Cada vez que se carga la pagina se ejecuta
 document.addEventListener("DOMContentLoaded", async () => {
-    //Aqui llamamos a nuestra funcion que se comunica con el backend para traer los datos de los cursos
-    const response = await getAllCategorias();
+  //Aqui llamamos a nuestra funcion que se comunica con el backend para traer los datos de los cursos
+  const response = await getAllCategorias();
 
-    const categorias = response.categories.map((categoria) => {
-        //Aqui se crea un arreglo con los datos que se van a mostrar en la tabla
-        // SI TIENEN DUDA: Son las propiedades que te regrese el objeto de POSTMAN
-        return [
-            categoria.id,
-            categoria.nombre,
-            categoria.descripcion,
-            categoria.imagenUrl,
-        ];
-    });
+  const categorias = response.categories.map((categoria) => {
+    //Aqui se crea un arreglo con los datos que se van a mostrar en la tabla
+    // SI TIENEN DUDA: Son las propiedades que te regrese el objeto de POSTMAN
+    return [
+      categoria.id,
+      categoria.nombre,
+      categoria.descripcion,
+      categoria.imagenUrl,
+    ];
+  });
 
-    // AHORA SI tenemos que crear la tabla con los datos que ya traemos
-    // Primero obtenemos el elemento de la tabla del html con el id
-    const categoriasTable = document.getElementById("categorias-table");
+  // AHORA SI tenemos que crear la tabla con los datos que ya traemos
+  // Primero obtenemos el elemento de la tabla del html con el id
+  const categoriasTable = document.getElementById("categorias-table");
 
-    //Aqui creamos la tabla con la libreria de DataTable
+  //Aqui creamos la tabla con la libreria de DataTable
 
-    const dataTable = new DataTable(categoriasTable, {
-        data: categorias,
-        columns: [
-            { title: "ID" },
-            { title: "Nombre" },
-            { title: "Descripcion" },
-            { title: "ImagenUrl" },
-            // Aqui se crea la columna de acciones, pueden agregar mas si quieren
-            // Son 2 botones, uno para editar y otro para eliminar
-            {
-                title: "Acciones",
-                render: function (data, type, row) {
-                    return `
+  const dataTable = new DataTable(categoriasTable, {
+    data: categorias,
+    columns: [
+      { title: "ID" },
+      { title: "Nombre" },
+      { title: "Descripcion" },
+      { title: "ImagenUrl" },
+      // Aqui se crea la columna de acciones, pueden agregar mas si quieren
+      // Son 2 botones, uno para editar y otro para eliminar
+      {
+        title: "Acciones",
+        render: function (data, type, row) {
+          return `
         <button class="btn btn-primary btn-sm" data-id="${row[0]}">
             EDITAR
         </button>
         <button class="btn btn-danger btn-sm" data-id="${row[0]}">
             ELIMINAR
         </button>
+        <button class="btn btn-warning btn-sm" data-id="${row[0]}">
+        ASIGNAR
+        </button>
       `;
-                },
-            },
-        ],
-
-        // Aqui se le da estilo a la tabla
-        // Este scrollX es para que se pueda hacer scroll horizontal y la pagina no se haga tan larga
-        scrollX: true,
-
-        // Aqui se cambia el lenguaje de la tabla a español
-        language: {
-            url: "//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json",
         },
-    });
+      },
+    ],
 
-    // AHORA agregamos los eventos a la tabla (tiene 2) uno para editar y otro para eliminar
+    // Aqui se le da estilo a la tabla
+    // Este scrollX es para que se pueda hacer scroll horizontal y la pagina no se haga tan larga
+    scrollX: true,
 
-    categoriasTable.addEventListener("click", async (e) => {
-        // Aqui se verifica que el boton que se dio click tenga la clase de btn-primary (que es el de EDITAR)
-        if (e.target.classList.contains("btn-primary")) {
-            // Aqui se obtiene el id del curso que se va a editar
-            const id = e.target.getAttribute("data-id");
+    // Aqui se cambia el lenguaje de la tabla a español
+    language: {
+      url: "//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json",
+    },
+  });
 
-            // Aqui encontramos los datos del curso que se va a editar
+  // AHORA agregamos los eventos a la tabla (tiene 2) uno para editar y otro para eliminar
 
-            const categoria = response.categories.find((categoria) => categoria.id == id);
+  categoriasTable.addEventListener("click", async (e) => {
+    // Aqui se verifica que el boton que se dio click tenga la clase de btn-primary (que es el de EDITAR)
+    if (e.target.classList.contains("btn-primary")) {
+      // Aqui se obtiene el id del curso que se va a editar
+      const id = e.target.getAttribute("data-id");
 
-            // Aqui se abre el modal de editar y se llenan los campos con los datos del curso
+      // Aqui encontramos los datos del curso que se va a editar
 
-            //Para hacer esto necesitamos todos los campos del modal
-            const nombre = document.getElementById("editar-nombre");
-            const descripcion = document.getElementById("editar-descripcion");
-            const imagenUrl = document.getElementById("editar-imagen");
+      const categoria = response.categories.find(
+        (categoria) => categoria.id == id
+      );
 
-            //Aqui se llenan los campos con los datos del curso
-            editarCategoriaModal.dataset.id = id;
-            nombre.value = categoria.nombre;
-            descripcion.value = categoria.descripcion;
-            imagenUrl.value = categoria.imagenUrl;
+      // Aqui se abre el modal de editar y se llenan los campos con los datos del curso
 
-            //Aqui se abre el modal
-            $("#editar-categoria-modal").modal("show");
+      //Para hacer esto necesitamos todos los campos del modal
+      const nombre = document.getElementById("editar-nombre");
+      const descripcion = document.getElementById("editar-descripcion");
+      const imagenUrl = document.getElementById("editar-imagen");
 
-            // MAS ABAJO AGREGAREMOS LOS EVENTOS PARA ACTUALIZAR LAS CATEGORIAS
-        }
+      //Aqui se llenan los campos con los datos del curso
+      editarCategoriaModal.dataset.id = id;
+      nombre.value = categoria.nombre;
+      descripcion.value = categoria.descripcion;
+      imagenUrl.value = categoria.imagenUrl;
 
-        // Ahora pasamos a editar el boton de ELIMINAR
-        if (e.target.classList.contains("btn-danger")) {
-            // Aqui se obtiene el id del curso que se va a eliminar
-            const id = e.target.getAttribute("data-id");
+      //Aqui se abre el modal
+      $("#editar-categoria-modal").modal("show");
 
-            // Aqui se muestra una alerta de confirmacion para eliminar el curso
-            const result = await Swal.fire({
-                title: "¿Estas seguro?",
-                text: "¡No podras revertir esto!",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonText: "¡Si, eliminar!",
-                cancelButtonText: "¡No, cancelar!",
-                reverseButtons: true,
+      // MAS ABAJO AGREGAREMOS LOS EVENTOS PARA ACTUALIZAR LAS CATEGORIAS
+    }
+
+    // Ahora pasamos a editar el boton de ELIMINAR
+    if (e.target.classList.contains("btn-danger")) {
+      // Aqui se obtiene el id del curso que se va a eliminar
+      const id = e.target.getAttribute("data-id");
+
+      // Aqui se muestra una alerta de confirmacion para eliminar el curso
+      const result = await Swal.fire({
+        title: "¿Estas seguro?",
+        text: "¡No podras revertir esto!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "¡Si, eliminar!",
+        cancelButtonText: "¡No, cancelar!",
+        reverseButtons: true,
+      });
+
+      // Si el usuario da click en el boton de SI, se ejecuta el codigo de abajo
+      if (result.isConfirmed) {
+        // Aqui se manda el id del curso que se va a eliminar al backend
+        // Aqui se puede usar la funcion que ya creamos para eliminar el curso
+        // O pueden crear una nueva, como gusten
+
+        // Aqui se muestra una alerta de que el curso se elimino correctamente
+        try {
+          await deleteCategories(id);
+
+          Swal.fire(
+            "¡Eliminado!",
+            "El curso se elimino correctamente.",
+            "success"
+          );
+
+          // Aqui se actualiza la tabla para que ya no aparezca la categoria eliminada
+
+          dataTable.row(e.target.parentElement.parentElement).remove().draw();
+        } catch (error) {
+          // ESTE MENSAJE SE UTILIZA EN TODOS LOS CATCH DE ESTE ARCHIVO
+          if (error.name === "ValidationError") {
+            const errorMessages = error.data.map((error) => {
+              return `<li>${error.msg}</li>`;
             });
-
-            // Si el usuario da click en el boton de SI, se ejecuta el codigo de abajo
-            if (result.isConfirmed) {
-                // Aqui se manda el id del curso que se va a eliminar al backend
-                // Aqui se puede usar la funcion que ya creamos para eliminar el curso
-                // O pueden crear una nueva, como gusten
-
-                // Aqui se muestra una alerta de que el curso se elimino correctamente
-                try {
-                    await deleteCategories(id);
-
-                    Swal.fire(
-                        "¡Eliminado!",
-                        "El curso se elimino correctamente.",
-                        "success"
-                    );
-
-                    // Aqui se actualiza la tabla para que ya no aparezca la categoria eliminada
-
-                    dataTable.row(e.target.parentElement.parentElement).remove().draw();
-                } catch (error) {
-                    // ESTE MENSAJE SE UTILIZA EN TODOS LOS CATCH DE ESTE ARCHIVO
-                    if (error.name === "ValidationError") {
-                        const errorMessages = error.data.map((error) => {
-                            return `<li>${error.msg}</li>`;
-                        });
-                        Swal.fire({
-                            title: "Validation Error",
-                            html: `<ul>${errorMessages.join("")}</ul>`,
-                            icon: "error",
-                        });
-                    } else {
-                        Swal.fire("Error", error.message, "error");
-                    }
-                }
-            }
+            Swal.fire({
+              title: "Validation Error",
+              html: `<ul>${errorMessages.join("")}</ul>`,
+              icon: "error",
+            });
+          } else {
+            Swal.fire("Error", error.message, "error");
+          }
         }
-    });
+      }
+    }
+
+    if (e.target.classList.contains("btn-warning")) {
+      const id = e.target.getAttribute("data-id");
+
+      const asignarCategoriaId = document.getElementById(
+        "asignar-categoria-id"
+      );
+
+      asignarCategoriaId.value = id;
+
+      $("#asignar-categoria-modal").modal("show");
+    }
+  });
 });
 
 //Ahora pasamos a agregar los eventos para actualizar Y crear un curso
@@ -173,100 +194,147 @@ const crearCategoriaForm = document.getElementById("crear-categoria-form");
 
 // Ahora agregamos los eventos para cuando se envie el formulario
 editarCategoriaForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    // obtenemos los datos del formulario
+  // obtenemos los datos del formulario
 
-    const id = editarCategoriaModal.dataset.id;
-    const nombre = editarCategoriaForm["editar-nombre"].value;
-    const descripcion = editarCategoriaForm["editar-descripcion"].value;
-    const imagenUrl = editarCategoriaForm["editar-imagen"].value;
+  const id = editarCategoriaModal.dataset.id;
+  const nombre = editarCategoriaForm["editar-nombre"].value;
+  const descripcion = editarCategoriaForm["editar-descripcion"].value;
+  const imagenUrl = editarCategoriaForm["editar-imagen"].value;
 
-    // AHORA SI hacemos la peticion al backend para actualizar el curso
+  // AHORA SI hacemos la peticion al backend para actualizar el curso
 
-    try {
-        const response = await updateCategories({
-            id,
-            nombre,
-            descripcion,
-            imagenUrl,
-        });
+  try {
+    const response = await updateCategories({
+      id,
+      nombre,
+      descripcion,
+      imagenUrl,
+    });
 
-        // Aqui se muestra una alerta de que el curso se actualizo correctamente
-        Swal.fire(
-            "¡Actualizado!",
-            `El curso ${response.categories.id} se actualizo correctamente`,
-            "success"
-        ).then(() => {
-            // Aqui se cierra el modal
-            $("#editar-categoria-modal").modal("hide");
+    // Aqui se muestra una alerta de que el curso se actualizo correctamente
+    Swal.fire(
+      "¡Actualizado!",
+      `El curso ${response.category.id} se actualizo correctamente`,
+      "success"
+    ).then(() => {
+      // Aqui se cierra el modal
+      $("#editar-categoria-modal").modal("hide");
 
-            // reiniciamos la pagina para que se actualice la tabla
+      // reiniciamos la pagina para que se actualice la tabla
 
-            window.location.reload();
-        });
-    } catch (error) {
-        // ESTE PEDAZO SE COPIA Y PEGA EN TODAS
-        if (error.name === "ValidationError") {
-            const errorMessages = error.data.map((error) => {
-                return `<li>${error.msg}</li>`;
-            });
-            Swal.fire({
-                title: "Validation Error",
-                html: `<ul>${errorMessages.join("")}</ul>`,
-                icon: "error",
-            });
-        } else {
-            Swal.fire("Error", error.message, "error");
-        }
-
-        //
+      window.location.reload();
+    });
+  } catch (error) {
+    // ESTE PEDAZO SE COPIA Y PEGA EN TODAS
+    if (error.name === "ValidationError") {
+      const errorMessages = error.data.map((error) => {
+        return `<li>${error.msg}</li>`;
+      });
+      Swal.fire({
+        title: "Validation Error",
+        html: `<ul>${errorMessages.join("")}</ul>`,
+        icon: "error",
+      });
+    } else {
+      Swal.fire("Error", error.message, "error");
     }
+
+    //
+  }
 });
 
 crearCategoriaForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const nombre = crearCategoriaForm["crear-nombre"].value;
-    const descripcion = crearCategoriaForm["crear-descripcion"].value;
-    const imagenUrl = crearCategoriaForm["crear-imagen"].value;
+  const nombre = crearCategoriaForm["crear-nombre"].value;
+  const descripcion = crearCategoriaForm["crear-descripcion"].value;
+  const imagenUrl = crearCategoriaForm["crear-imagen"].value;
 
-    // Aqui se hace la peticion al backend para crear el curso
+  // Aqui se hace la peticion al backend para crear el curso
 
-    try {
-        const response = await createCategories({
-            nombre,
-            descripcion,
-            imagenUrl,
-        });
+  try {
+    const response = await createCategories({
+      nombre,
+      descripcion,
+      imagenUrl,
+    });
 
-        // Aqui se muestra una alerta de que el curso se creo correctamente
+    // Aqui se muestra una alerta de que el curso se creo correctamente
 
-        Swal.fire(
-            "¡Creado!",
-            `El curso ${response.categories.id} se creo correctamente`,
-            "success"
-        ).then(() => {
-            // Aqui se cierra el modal
-            $("#crear-categoria-modal").modal("hide");
-            $("#crear-categoria-form").trigger("reset");
+    Swal.fire(
+      "¡Creado!",
+      `El curso ${response.category.id} se creo correctamente`,
+      "success"
+    ).then(() => {
+      // Aqui se cierra el modal
+      $("#crear-categoria-modal").modal("hide");
+      $("#crear-categoria-form").trigger("reset");
 
-            // reiniciamos la pagina para que se actualice la tabla
+      // reiniciamos la pagina para que se actualice la tabla
 
-            window.location.reload();
-        });
-    } catch (error) {
-        if (error.name === "ValidationError") {
-            const errorMessages = error.data.map((error) => {
-                return `<li>${error.msg}</li>`;
-            });
-            Swal.fire({
-                title: "Validation Error",
-                html: `<ul>${errorMessages.join("")}</ul>`,
-                icon: "error",
-            });
-        } else {
-            Swal.fire("Error", error.message, "error");
-        }
+      window.location.reload();
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      const errorMessages = error.data.map((error) => {
+        return `<li>${error.msg}</li>`;
+      });
+      Swal.fire({
+        title: "Validation Error",
+        html: `<ul>${errorMessages.join("")}</ul>`,
+        icon: "error",
+      });
+    } else {
+      Swal.fire("Error", error.message, "error");
     }
+  }
+});
+
+// Asignar categoria a un curso
+
+const asignarCategoriaForm = document.getElementById("asignar-categoria-form");
+
+asignarCategoriaForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const categoria_id = Number(
+    asignarCategoriaForm["asignar-categoria-id"].value
+  );
+
+  const curso_id = Number(document.getElementById("asignar-curso-id").value);
+
+  try {
+    const response = await asignarCategoria({
+      categoria_id,
+      curso_id,
+    });
+
+    Swal.fire({
+      title: "¡Asignado!",
+      text: `La categoria ${response.cursoCategory.categoria_id} se asigno correctamente al curso ${response.cursoCategory.curso_id}`,
+    }).then(() => {
+      // Aqui se cierra el modal
+      $("#asignar-categoria-modal").modal("hide");
+      $("#asignar-categoria-form").trigger("reset");
+
+      // reiniciamos la pagina para que se actualice la tabla
+
+      window.location.reload();
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      const errorMessages = error.data.map((error) => {
+        return `<li>${error.msg}</li>`;
+      });
+      Swal.fire({
+        title: "Validation Error",
+        html: `<ul>${errorMessages.join("")}</ul>`,
+        icon: "error",
+      });
+    } else {
+      Swal.fire("Error", error.message, "error");
+    }
+  }
 });
